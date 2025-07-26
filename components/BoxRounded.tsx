54f49468f8c7005f2e0b2e8a36c7e725a565ddcb
julius-ph <71057113+juliusL03@users.noreply.github.com>
2025-07-26 16:52:32 +0800

    added user info and trademark

--- components/BoxRounded.tsx
+++ components/BoxRounded.tsx
@@ -6,7 +6,7 @@ type props = {
 
 export default function BoxRounded({ children }: props) {
   return (
-    <div className="md:w-2/3 flex flex-col md:flex-col py-4 md:pr-4 gap-4 md:border-r-2 md:border-gray-100/30">
+    <div className="md:w-2/3 h-screen flex flex-col py-4 md:pr-4 gap-4 md:border-r-2 md:border-gray-100/30">
       {children}
     </div>
   );
---
+++ components/TradeMark.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function TradeMark() {
+  return (
+    <div className="text-center text-xs text-gray-500">
+      Developed by{" "}
+      <a
+        href="https://www.julsdev.site/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline text-blue-500"
+      >
+        Juls Dev
+      </a>
+    </div>
+  );
+}
